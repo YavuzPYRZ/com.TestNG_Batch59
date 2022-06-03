3d77e994879c2c2c8355d9569a88d5cae2989765
YavuzPYRZ <yavuzpoyraz03@gmail.com>
2022-06-03 20:11:54 +0200

    03.05

--- src/test/java/pages/BrcPage.java
+++ src/test/java/pages/BrcPage.java
@@ -1,26 +1,21 @@
 package pages;
-
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
-
 public class BrcPage {
-    public BrcPage(){
-
-        PageFactory.initElements(Driver.getDriver(),this);
-    }
-    @FindBy(xpath = "//a[@class='btn btn-primary btn-sm']")
+    public BrcPage() {   //bir page class'i olusturuldugunda yapilmasi gereken ilk sey bir constructor olusturmak ve
+        //   diger classlardan ulasabilmek icin public yapariz
+        PageFactory.initElements(Driver.getDriver(), this);   //ve driver'imizin bu class'ta gecerli oldugunu soylemek
+    }  //bundaan sonra da istedigimiz locate'leri @FindBy notasyonu ile yapmaliyiz
+    @FindBy(xpath = "(//a[@role='button'])[1]")
     public WebElement ilkLoginButonu;
-
-    @FindBy(xpath = "//input[@id='formBasicEmail']")
+    @FindBy(xpath = "//input[@name='email']")
     public WebElement emailTextBox;
-
     @FindBy(xpath = "//input[@name='password']")
     public WebElement passwordTextBox;
-
     @FindBy(xpath = "//button[@type='submit']")
     public WebElement ikinciLoginButonu;
-
-
+    @FindBy(id = "dropdown-basic-button")
+    public WebElement kullaniciProfilIsmi;
 }
---
+++ src/test/java/tests/day21_reusableMethods_HtmlReports/C03_PositiveLoginTestiRaporlu.java
@@ -0,0 +1,5 @@
+package tests.day21_reusableMethods_HtmlReports;
+
+public class C03_PositiveLoginTestiRaporlu {
+
+}
